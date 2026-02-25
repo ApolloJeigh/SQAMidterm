7c342dde5112d847908126c9499a76d13b07bb63
ApolloJeigh <cam.henderson93@gmail.com>
2026-02-25 12:48:06 -0500

    first commit

---
+++ ConcreteTests/ConcreteTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MidtermQ1;
+
+namespace ConcreteTests
+{
+    public class ConcreteTest
+    {
+
+        [Test]
+        public void CalculateCost_NewResidential1SquareMeter_Returns20()
+        {
+            //Arrange
+            double expected = 20;
+
+            //Act
+            double actual = ConcreteCostCalculator.CalculateCost(CustomerType.NewResidential, 1);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CalculateCost_ExistingResidential1SquareMeter_Returns15()
+        {
+            //Arrange
+            double expected = 15;
+
+            //Act
+            double actual = ConcreteCostCalculator.CalculateCost(CustomerType.ExistingResidential, 1);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CalculateCost_Commercial1SquareMeter_Returns10()
+        {
+            //Arrange
+            double expected = 10;
+
+            //Act
+            double actual = ConcreteCostCalculator.CalculateCost(CustomerType.Commercial, 1);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CalculateCost_Commericial10SquareMeters_Return100()
+        {
+            //Arrange
+            double expected = 100;
+
+            //Act
+            double actual = ConcreteCostCalculator.CalculateCost(CustomerType.Commercial, 10);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CalculateCost_Commericial11SquareMeters_ReturnNegative2()
+        {
+            //Arrange
+            double expected = -2;
+
+            //Act
+            double actual = ConcreteCostCalculator.CalculateCost(CustomerType.Commercial, 11);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ MidtermQ1/ConcreteCostCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermQ1
+{
+    public enum CustomerType
+    {
+        NewResidential,
+        ExistingResidential,
+        Commercial
+    }
+
+    public class ConcreteCostCalculator
+    {
+        private double squareMeters;
+        private CustomerType customerType;
+
+        public ConcreteCostCalculator() { }
+        public static double CalculateCost(CustomerType customerType, double squareMeters)
+        {
+            // check concrete squareMeters
+            if ((squareMeters * 1000) < 1000.00) return -1.00;  // -1.00 signifies concrete level TOO LOW
+            if ((squareMeters * 1000) > 10000.00) return -2.00; // -2.00 signifies concrete level TOO HIGH
+
+            double rate = GetRate(customerType);
+            return squareMeters * rate;
+        }
+
+        private static double GetRate(CustomerType customerType)
+        {
+            switch (customerType)
+            {
+                case CustomerType.NewResidential:
+                    return 20;
+                case CustomerType.ExistingResidential:
+                    return 15;
+                case CustomerType.Commercial:
+                    return 10;
+                default:
+                    throw new ArgumentException("Invalid customer type");
+            }
+        }
+    }
+
+}
